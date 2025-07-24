670957356abc2dbf7bc933e1b0a6ee5fe65c94b7
AnonymousGost946 <anonymousghost946@gmail.com>
2025-07-24 10:41:41 -0400

    I push my project page on github 23-july-2025

---
+++ scripts/project-page.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",function () {
+    
+        gsap.registerPlugin(ScrollTrigger);
+
+    const sections = document.querySelectorAll(".project-card");
+    const container = document.querySelector(".projects");
+
+    const totalScroll = container.scrollWidth - window.innerWidth;
+
+    gsap.to(container, {
+      x: () => -totalScroll,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".project-container",
+        start: "top top",
+        end: () => "+=" + totalScroll,
+        pin: true,
+        scrub: true,
+        anticipatePin: 1,
+        markers: false, // remove when done
+      }
+    });
+})
